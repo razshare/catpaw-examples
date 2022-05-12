81599e22c0ad484ffb6301f63ad4a5686677c1a2
Razvan <tangent.jotey@gmail.com>
2022-05-12 14:38:05 +0200

    Update main.php

--- src/svelte/main.php
+++ src/svelte/main.php
@@ -2,17 +2,76 @@
 
 namespace {
 
-	use CatPaw\Web\Attribute\StartWebServer;
-	use CatPaw\Web\Utility\Route;
+	use Amp\ByteStream\ClosedException;
+	use Amp\ByteStream\StreamException;
+	use Amp\CancelledException;
+	use Amp\Process\Process;
+	use Amp\Socket\ConnectException;
+	use CatPaw\Svelte\lib\Exception\SvelteException;
+	use CatPaw\Svelte\lib\Service\RequestEncoderService;
+	use CatPaw\Svelte\lib\Service\SvelteService;
+	use CatPaw\Svelte\lib\SvelteExchanger;
+	use CatPaw\Utility\Strings;
 
-	#[StartWebServer]
-	function main() {
-		echo "Server started";
+	/**
+	 * @param SvelteService         $svelte
+	 * @param RequestEncoderService $encoder
+	 * @return Generator
+	 * @throws StreamException
+	 * @throws SvelteException
+	 * @throws ClosedException
+	 * @throws CancelledException
+	 * @throws ConnectException
+	 */
+	function main(
+		SvelteService         $svelte,
+		RequestEncoderService $encoder,
+	): Generator {
+		$hostname = "127.0.0.1";
+		$port = 5757;
+//		$secret = Strings::uuid();
+		$secret = "secret";
+		$dump = "./providers/tmp";
 
-		Route::get("/", function(
+		/** @var Process $process */
 
-		) {
-			return "hello world";
+		[
+			"process"   => $process,
+			"secret"    => $secret,
+		] = yield $svelte->start($hostname, $port, $secret, $dump, function() {
+			echo "Process is over.\n";
 		});
+
+		/** @var SvelteExchanger $svelte */
+		try {
+			$svelte = yield $svelte->connect($hostname, $port, $secret);
+
+			$source = $encoder
+				->setGenerate('ssr')
+				->setFormat('esm')
+				->setProperties([
+									"name" => "test",
+								])
+				->setBody(<<<SVELTE
+				<script>
+					export let name = 'world'
+				</script>
+				<!-- this is a comment -->
+				<b>hello {name}</b>
+				SVELTE
+				)
+				->build();
+
+			echo yield $svelte->ssr($source);
+			echo PHP_EOL;
+		} catch(Throwable $e) {
+			echo $e->getMessage().PHP_EOL;
+		}
+
+//		try {
+//			$process->kill();
+//		} catch(Throwable $e) {
+//			echo $e->getMessage();
+//		}
 	}
 }
