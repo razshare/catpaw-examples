18518c50b5accca0cf8df109a769bbe71185a521
tncrazvan <tangent.jotey@gmail.com>
2023-06-15 19:41:40 +0200

    Migrating to fibers.

--- src/stores/readable/main.php
+++ src/stores/readable/main.php
@@ -1,25 +1,23 @@
 <?php
 
 namespace {
-    use function Amp\async;
     use function Amp\delay;
     use function CatPaw\Store\readable;
 
     function main() {
         $time = readable(time(), function($set) {
-            ticktock(5, fn () => $set(time()));
+            // ticktock(5, fn () => $set(time()));
+            $set(time());
         });
         $time->subscribe(fn ($time) => print("the time is $time\n"));
     }
 
     function ticktock(int $iterations, callable $callback) {
-        async(function() use (&$iterations, $callback) {
-            delay(1);
-            $callback();
-            $iterations--;
-            if ($iterations > 0) {
-                ticktock($iterations, $callback);
-            }
-        });
+        delay(1);
+        $callback();
+        $iterations--;
+        if ($iterations > 0) {
+            ticktock($iterations, $callback);
+        }
     }
 }
