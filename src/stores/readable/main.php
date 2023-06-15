199a133c27b25fa845fd367ab43bf5ae409ceda6
tncrazvan <tangent.jotey@gmail.com>
2023-06-15 20:54:14 +0200

    Version 0.7.15

--- src/stores/readable/main.php
+++ src/stores/readable/main.php
@@ -5,7 +5,7 @@ use function CatPaw\Store\readable;
 
 function main() {
     $time = readable(time(), function($set) {
-        // ticktock(5, fn () => $set(time()));
+        ticktock(5, fn () => $set(time()));
         $result = $set(time());
     });
     $time->subscribe(fn ($time) => print("the time is $time\n"));
